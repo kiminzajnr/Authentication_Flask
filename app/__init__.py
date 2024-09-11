a01671ac7c0966f7ba431857aa03f59a66b2b2cb
kiminzajnr <kiminzaeric@gmail.com>
2024-09-11 15:24:18 +0300

    Add authentication and test

--- app/__init__.py
+++ app/__init__.py
@@ -9,31 +9,59 @@ from flask import (
     redirect,
     url_for,
 )
+from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 # Secret key generated with secrets.token_urlsafe()
 app.secret_key = "lkaQT-kAb6aIvqWETVcCQ28F-j-rP_PSEaCDdTynkXA"
 
+# Use MongoDB to store users
 users = {}
 
-
+# Protect this endpont to be accessible to only logged users
+# Tutorial https://blog.teclado.com/decorators-in-python/
 @app.get("/")
 def home():
     return render_template("home.html", email=session.get("email"))
 
 
+# Protect this endpont to be accessible to only logged users
 @app.get("/protected")
 def protected():
     if not session.get("email"):
-        abort(401)
+        abort(401, "User not authenticated")
     return render_template("protected.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if pbkdf2_sha256.verify(password, users.get(email)):
+            session["email"] = email
+            return redirect(url_for("protected"))
+        abort(401, "incorrect Email or password")
+
+
     return render_template("login.html")
 
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
 
-@app.route("/signup", methods=["GET"])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        users[email] = pbkdf2_sha256.hash(password)
+        print(users)
+        flash("Signed up successfully.")
+        return redirect(url_for("login"))
+
     return render_template("signup.html")
